50830c7264df7aabccb7fc23d70cff9a0906e814
dudualencar <dudu10.alencar@gmail.com>
2021-12-11 08:33:41 -0300

    Atualização 1.2

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,18 @@ const webpack = require('webpack');
  
 module.exports = {
   mode: 'development',
-  entry: "./src/principal.js"
+  entry: "./src/principal.js",
+  output: {
+    filename: 'principal.js',
+    path: __dirname + '/public'
+  },
+  module: [{
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader', //Adicionar CSS a Dom injetando a tag <style>
+        'css-loader' //interpreta @import, url()...
+      ]
+    }]
+  }]
 }  
